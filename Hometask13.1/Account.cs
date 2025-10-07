152913f151e8e2f781d67d0b7ca05bb19bc72b5c
Natalia Holub <nata.holub@ukr.net>
2025-10-07 19:30:11 +0300

    Hometask

---
+++ Hometask13.1/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask13._1
+{
+    public class Account
+    {
+        private string accountName;
+        private decimal balance;
+
+        public decimal Balance => balance;
+
+        public Account(string accountName, decimal balance)
+        {
+            this.accountName = accountName;
+            this.balance = balance;
+        }
+
+        public void Withdrawal(decimal amountOfMoney)
+        {
+            if (amountOfMoney <= balance)
+            {
+                balance -= amountOfMoney;
+            }
+            else
+            {
+                Console.WriteLine("Transaction declined: insufficient funds.");
+            }
+        }
+        public void Deposit(decimal amountOfMoney)
+        {
+            balance += amountOfMoney;
+        }
+    }
+}
---
+++ Hometask13.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Hometask13._1;
+
+Account privatBankAccount = new Account("PrivatBank account", 100.00m);
+Account monoBankAccount = new Account("MonoBank account", 2000.00m);
+
+privatBankAccount.Withdrawal(50);
+Console.WriteLine("The balance of PrivatBank account is now: " + privatBankAccount.Balance);
+
+privatBankAccount.Deposit(100);
+Console.WriteLine("The balance of PrivatBank account is now: " + privatBankAccount.Balance);
+
+privatBankAccount.Withdrawal(160);
+Console.WriteLine("The balance of PrivatBank account is now: " + privatBankAccount.Balance);
+
+Console.WriteLine();
+
+monoBankAccount.Deposit(500);
+Console.WriteLine("The balance of MonoBank account is now: " + monoBankAccount.Balance);
+
+monoBankAccount.Withdrawal(1500);
+Console.WriteLine("The balance of MonoBank account is now: " + monoBankAccount.Balance);
+
+monoBankAccount.Withdrawal(1100);
+Console.WriteLine("The balance of MonoBank account is now: " + monoBankAccount.Balance);
